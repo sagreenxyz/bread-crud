4b56b877d3e30c3811539af30fa8a65f4d231dc6
Scott A. Green <sagreenxyz@gmail.com>
2022-08-31 15:59:26 -0500

    setting up body parsing for urlencoded

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const PORT = process.env.PORT
 const express = require('express')
 const app = express()
 
+app.use(express.urlencoded({ extended: true }))
+
 app.use(express.static('public'))
 
 app.set('views', __dirname + '/views')
