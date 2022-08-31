7dc895d7d520bdca3a48b849e2361e603826d6ce
Scott A. Green <sagreenxyz@gmail.com>
2022-08-31 17:22:39 -0500

    setting defaults for name and image fields in breads schema

--- models/bread.js
+++ models/bread.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose
 
 const breadSchema = new Schema({
-    name: { type: String },
+    name: { type: String, required: true },
     hasGluten: { type: Boolean },
-    image: { type: String }
+    image: { type: String, default: 'https://via.placeholder.com/500' }
 })
 
 const Bread = mongoose.model('Bread', breadSchema)
