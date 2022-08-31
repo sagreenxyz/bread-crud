3e01f08e12d34b1afb89bb2f985edf4343a4dfc6
Scott A. Green <sagreenxyz@gmail.com>
2022-08-31 15:13:18 -0500

    adding conditional rendering to show whether or not has gluten

--- views/show.jsx
+++ views/show.jsx
@@ -7,6 +7,13 @@ function Show({ bread }) {
         <Default>
             <h2>Show Page</h2>
             <h3>{bread.name}</h3>
+            <p>
+                and it {
+                    bread.hasGluten
+                    ? <span>does</span>
+                    : <span>does NOT</span>
+                } have gluten.
+            </p>
             <img src={bread.image} alt={bread.name} width="300px" />
         </Default>
     )
