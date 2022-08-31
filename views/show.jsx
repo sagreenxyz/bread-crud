e2949fb28d5814058ebc4ef8006378a16be7dba7
Scott A. Green <sagreenxyz@gmail.com>
2022-08-31 16:43:52 -0500

    adding edit button to specific bread show page

--- views/show.jsx
+++ views/show.jsx
@@ -15,6 +15,7 @@ function Show({ bread, index }) {
                 } have gluten.
             </p>
             <img src={bread.image} alt={bread.name} width="300px" />
+            <a href={`/breads/${index}/edit`}><button>Edit</button></a>
             <form action={`/breads/${index}?_method=DELETE`} method="POST">
                 <input type='submit' value="DELETE" />
             </form>
