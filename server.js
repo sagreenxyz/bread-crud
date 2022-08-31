c779387ea74626e4fbd58e6ed76338e830933b90
Scott A. Green <sagreenxyz@gmail.com>
2022-08-31 12:48:30 -0500

    views folder, view engine, and default template

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const PORT = process.env.PORT
 const express = require('express')
 const app = express()
 
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads!')
 })
