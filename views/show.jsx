2265369ee8801ed216299c4181d8f4ebeb18cc47
Scott A. Green <sagreenxyz@gmail.com>
2022-08-31 15:11:11 -0500

    adding more details to the specific bread show view

--- views/show.jsx
+++ views/show.jsx
@@ -6,6 +6,8 @@ function Show({ bread }) {
     return (
         <Default>
             <h2>Show Page</h2>
+            <h3>{bread.name}</h3>
+            <img src={bread.image} alt={bread.name} width="300px" />
         </Default>
     )
 }
