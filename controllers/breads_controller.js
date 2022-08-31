186414b55da7c378bc51448816899cd5d4318374
Scott A. Green <sagreenxyz@gmail.com>
2022-08-31 17:19:26 -0500

    removing mock data from breads model and setting up bread schema

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const breads = express.Router()
-const Bread = require('../models/bread.js')
+const Bread = require('../models/bread-bak.js')
 
 breads.get('/', (req, res) => {
     res.render('index', {
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 require('dotenv').config()
 const PORT = process.env.PORT
 
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true },
+    () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+)
+
 const express = require('express')
 const app = express()
 
