5d843185a07847fe439e546d0c95655af65bfa9b
Scott A. Green <sagreenxyz@gmail.com>
2022-08-31 20:42:17 -0500

    creating the bakers controller

---
+++ controllers/bakers_controller.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const baker = express.Router()
+const Baker = require('../models/baker.js')
+const bakerSeedData = require('../models/baker_seed.js')
+
+module.exports = baker                    
