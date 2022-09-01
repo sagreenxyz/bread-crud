ffab2c4977b7bb5936aab52a7738cfe2cc1c6942
Scott A. Green <sagreenxyz@gmail.com>
2022-08-31 20:49:49 -0500

    using baker prop in new bread form for baker

--- views/new.jsx
+++ views/new.jsx
@@ -1,7 +1,7 @@
 const React = require('react')
 const Default = require('./layouts/default')
 
-function New() {
+function New({ bakers }) {
     return (
         <Default>
             <h2>Add a New Bread</h2>
@@ -28,12 +28,11 @@ function New() {
                 <br />
                 <label htmlFor="baker">Baker</label>
                 <select name="baker" id="baker">
-                    <option value="Rachel">Rachel</option>
-                    <option value="Monica">Monica</option>
-                    <option value="Joey">Joey</option>
-                    <option value="Chandler">Chandler</option>
-                    <option value="Ross">Ross</option>
-                    <option value="Phoebe">Phoebe</option>
+                    {bakers.map(baker => {
+                        return (
+                            <option value={baker.id} key={baker.id}>{baker.name}</option>
+                        )
+                    })}
                 </select>
                 <input type="submit" />
             </form>
