1185714aca926984044d86460e14603f5e12146b
Scott A. Green <sagreenxyz@gmail.com>
2022-08-31 21:44:59 -0500

    completing the foreign document delete - deleting all breads of the specific baker being deleted

--- models/baker.js
+++ models/baker.js
@@ -23,9 +23,12 @@ bakerSchema.virtual('breads', {
     foreignField: 'baker'
 })
 
-bakerSchema.post('findOneAndDelete', function() {
-    console.log(this)
-})            
+bakerSchema.post('findOneAndDelete', function () {
+    Bread.deleteMany({ baker: this._conditions._id })
+        .then(deleteStatus => {
+            console.log(deleteStatus)
+        })
+})
 
 const Baker = mongoose.model('Baker', bakerSchema)
 
