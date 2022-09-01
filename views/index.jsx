b014ee503629bfb446eb9b47fbdf5cf92a0363ed
Scott A. Green <sagreenxyz@gmail.com>
2022-08-31 21:59:14 -0500

    modifying breads index view to convert id values to _id values from database documents

--- views/index.jsx
+++ views/index.jsx
@@ -10,8 +10,8 @@ function Index({ breads, bakers, title }) {
                 {
                     bakers.map((baker) => {
                         return (
-                            <li key={baker.id}>
-                                <a href={`/bakers/${baker.id}`}>{baker.name}</a>
+                            <li key={baker._id}>
+                                <a href={`/bakers/${baker._id}`}>{baker.name}</a>
                             </li>
                         )
                     })
@@ -21,7 +21,7 @@ function Index({ breads, bakers, title }) {
             <ul>{
                 breads.map((bread, index) => {
                     return (
-                        <li key={index}>
+                        <li key={bread._id}>
                             <a href={`/breads/${bread._id}`}>
                                 {bread.name}
                             </a>
