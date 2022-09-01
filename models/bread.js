8b307a32e05d872cee33ff2a94d0b91221317915
Scott A. Green <sagreenxyz@gmail.com>
2022-08-31 20:58:25 -0500

    adding baker year started to getBakedBy helper method

--- models/bread.js
+++ models/bread.js
@@ -12,7 +12,7 @@ const breadSchema = new Schema({
 })
 
 breadSchema.methods.getBakedBy = function () {
-  return `${this.name} was baked with love by ${this.baker.name}`
+  return `${this.name} was baked with love by ${this.baker.name}, who has been with us since ${this.baker.startDate.getFullYear()}.`
 }
 
 const Bread = mongoose.model('Bread', breadSchema)
