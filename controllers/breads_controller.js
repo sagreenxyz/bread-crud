64a25092a4e2cd03a5991867593d0a47a3a205a8
Scott A. Green <sagreenxyz@gmail.com>
2022-08-31 19:41:52 -0500

    using the instance method in the bread show page

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -58,6 +58,8 @@ breads.get('/:id/edit', (req, res) => {
 breads.get('/:id', (req, res) => {
     Bread.findById(req.params.id)
         .then(foundBread => {
+            const bakedBy = foundBread.getBakedBy()
+            console.log(bakedBy)
             res.render('show', {
                 bread: foundBread
             })
