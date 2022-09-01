c5d3a0c4869d551fd3bff325ea45df7167e52653
Scott A. Green <sagreenxyz@gmail.com>
2022-08-31 19:39:09 -0500

    creating an instance method in the bread schema

--- models/bread.js
+++ models/bread.js
@@ -2,14 +2,18 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose
 
 const breadSchema = new Schema({
-    name: { type: String, required: true },
-    hasGluten: { type: Boolean },
-    image: { type: String, default: 'https://via.placeholder.com/500' },
-    baker: {
-      type: String,
-      enum: ['Rachel', 'Monica', 'Joey', 'Chandler', 'Ross', 'Phoebe']
-    }
+  name: { type: String, required: true },
+  hasGluten: { type: Boolean },
+  image: { type: String, default: 'https://via.placeholder.com/500' },
+  baker: {
+    type: String,
+    enum: ['Rachel', 'Monica', 'Joey', 'Chandler', 'Ross', 'Phoebe']
+  }
 })
+
+breadSchema.methods.getBakedBy = function () {
+  return `${this.name} was baked with love by ${this.baker}`
+}
 
 const Bread = mongoose.model('Bread', breadSchema)
 
